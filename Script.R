ec4ee5c3058ccfad9edfdc83d5d5f0e10b750634
Delta9-bit <57607361+Delta9-bit@users.noreply.github.com>
2020-10-02 12:38:25 +0200

    Update Script.R

--- Script.R
+++ Script.R
@@ -1,9 +1,29 @@
 library(rugarch)
 library(readxl)
+library(ggplot2)
 
-setwd ("/Users/lucasA/Desktop/Econométrie Financière")
+setwd ("/Users/lucasA/Desktop/Econométrie Financière/Econometrie-Financiere")
 
 DataSP500 <- read.csv("Data/SP500.CSV")[, c(1, 5,6)]
 
+DataSP500$Date <- as.Date(DataSP500$Date)
+
 head(DataSP500)
 
+DataSP500[, c(2, 3)] <- log(DataSP500[, c(2, 3)])
+
+ggplot(data = DataSP500, aes(x = Date, y = Adj.Close))+
+  geom_line(color = 'navyblue')+
+  theme_minimal()
+
+R <- diff(DataSP500[, 3])
+
+DataSP500 <- DataSP500[-c(1), ]
+
+DataSP500$R <- R 
+DataSP500$Vol <- R^2
+
+ggplot(data = DataSP500, aes(x = Date, y = R))+
+  geom_line(color = 'navyblue')+
+  geom_line(aes(y = Vol), color = 'red')+
+  theme_minimal()
