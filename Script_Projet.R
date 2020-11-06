686bb0f78988eed9dacb1552331175eb8a7acdb0
Delta9-bit <lucas.aurouet@hotmail.fr>
2020-11-06 12:57:18 +0100

    adding models

--- Script_Projet.R
+++ Script_Projet.R
@@ -11,48 +11,110 @@ library(robustbase)
 library(rugarch)
 library(GAS)
 
-data <- zoo(diff(log(as.numeric(read.csv("Data/AF.PA.csv")[, c(6)]))))
-data_df <- data.frame('Series' = diff(log(as.numeric(read.csv("Data/AF.PA.csv")[, c(6)])), 'index' = seq (1 : length(data))))
+# importing data
+
+data <- zoo(diff(log(as.numeric(read.csv("Data/WTI.csv")[, c(2)]))))
+data_df <- data.frame('series' = as.numeric(read.csv("Data/WTI.csv")[, c(2)]))
+data_df$index <- seq(1, nrow(data_df), 1)
+returns <- diff(log(data_df$series))
+data_df <- data_df[- c(1),]
+data_df$returns <- returns
+data_ts <- ts(data_df[3])
+
+# Outliers removal
+
+data_clean <- Return.clean(data_ts, method = 'boudt')
+data_df$clean <- as.numeric(data_clean)
+
+rm(returns)
+
+# Series plots
+
+ggplot(aes(x = index), data = data_df)+
+  geom_line(aes(y = returns),color = 'red')+
+  geom_line(aes(y = clean), color = 'steelblue')
+  theme_minimal()
+  
+ggplot(mapping = aes(x = index, y = series), data_df)+
+  geom_line(color = 'steelblue')+
+  theme_minimal()
+
+ggplot(mapping = aes(x = index), data_df)+
+  geom_line(aes(y = returns^2),color = 'steelblue')+
+  geom_line(aes(y = clean^2),color = 'red')+
+  theme_minimal()
+
+# ACF/PACF plots
+par(mfrow = c(2 ,2))
+acf(data_df$returns, main = 'acf returns')
+pacf(data_df$returns, main = 'pacf returns')
+acf(data_df$returns^2, main = 'acf returns^2')
+pacf(data_df$returns^2, main = 'pacf returns^2')
+dev.off()
+
+# Summary statisitcs
 
 FinTS.stats(data)
 
 basicStats(data)
 
-data <- ts(data)
+summary(data_ts)
 
-summary(data)
+SemiDeviation(data_ts)
 
-SemiDeviation(data)
+JarqueBeraTest(data_ts)
 
-JarqueBeraTest(data)
+Box.test(data_ts, lag = 10, type = c('Ljung-Box'))
+Box.test(data_ts^2, lag = 10, type = c('Ljung-Box'))
 
-Box.test(data, lag = 10, type = c('Ljung-Box'))
-Box.test(data^2, lag = 10, type = c('Ljung-Box'))
+ArchTest(data_ts^2, lag = 10)
 
-ArchTest(data^2, lag = 10)
-
-ggplot(aes(x = index, y = Series), data = data_df)+
-  geom_line(color = 'navyblue')+
-  theme_minimal()
-
-par(mfrow = c(2 ,2))
-  acf(data)
-  pacf(data)
-  acf(data^2)
-  pacf(data^2)
-  
 h <- hist(data, col = 'navyblue')
 xfit <- seq(min(data), max(data), length = 40)
 yfit <- dnorm(xfit, mean = mean(data), sd = sd(data))
 yfit <- yfit * diff(h$mids[1 : 2]) * length(data)
 lines(xfit, yfit, col = 'red', lwd = 2)
 
-data <- Return.clean(data, method = 'boudt')
+# GARCH models with norm and st distributions
 
-sGARCH_spec <- ugarchspec(variance.model = list(model = 'sGARCH'), 
+GARCHspec <- ugarchspec(variance.model = list(model = 'sGARCH', garchOrder = c(1, 1)), 
+                        mean.model = list(armaOrder = c(0, 0), include.mean = TRUE), distribution.model = 'norm')
+GARCHspec_st <- ugarchspec(variance.model = list(model = 'sGARCH', garchOrder = c(1, 1)), 
+                        mean.model = list(armaOrder = c(0, 0), include.mean = TRUE), distribution.model = 'std')
+GARCHfit <- ugarchfit(GARCHspec, data = data) # Standard GARCH(1, 1) spec and fit
+GARCHfit <- ugarchfit(GARCHspec_st, data = data)
+
+iGARCHspec <- ugarchspec(variance.model = list(model = 'iGARCH', garchOrder = c(1, 1)), 
+                        mean.model = list(armaOrder = c(0, 0), include.mean = TRUE), distribution.model = 'norm')
+iGARCHspec_st <- ugarchspec(variance.model = list(model = 'iGARCH', garchOrder = c(1, 1)), 
+                         mean.model = list(armaOrder = c(0, 0), include.mean = TRUE), distribution.model = 'std')
+iGARCHfit <- ugarchfit(iGARCHspec, data = data) # Integrated GARCH(1, 1) spec and fit 
+iGARCHfit <- ugarchfit(iGARCHspec_st, data = data)
+
+eGARCHspec <- ugarchspec(variance.model = list(model = 'eGARCH', garchOrder = c(1, 1)),
+                        mean.model = list(armaOrder = c(0, 0), include.mean = TRUE), distribution.model = 'norm')
+eGARCHspec_st <- ugarchspec(variance.model = list(model = 'eGARCH', garchOrder = c(1, 1)),
+                         mean.model = list(armaOrder = c(0, 0), include.mean = TRUE), distribution.model = 'std')
+eGARCHfit <- ugarchfit(eGARCHspec, data = data) # Exponential GARCH(1, 1) spec and fit 
+eGARCHfit <- ugarchfit(eGARCHspec_st, data = data)
+
+gjrGARCHspec <- ugarchspec(variance.model = list(model = 'gjrGARCH', garchOrder = c(1, 1)),
+                           mean.model = list(armaOrder = c(0, 0), include.mean = TRUE), distribution.model = 'norm')
+gjrGARCHspec_st <- ugarchspec(variance.model = list(model = 'gjrGARCH', garchOrder = c(1, 1)),
+                           mean.model = list(armaOrder = c(0, 0), include.mean = TRUE), distribution.model = 'std')
+gjrGARCHfit <- ugarchfit(gjrGARCHspec, data = data) # GJR GARCH(1, 1) spec and fit 
+gjrGARCHfit <- ugarchfit(gjrGARCHspec_st, data = data)
+
+tGARCHspec <- ugarchspec(variance.model = list(model = 'tGARCH', garchOrder = c(1, 1)),
                          mean.model = list(armaOrder = c(0, 0), include.mean = TRUE), distribution.model = 'norm')
-sGARCH_fit <- ugarchfit(spec = sGARCH_spec, data = data)
-sGARCH_fit
+tGARCHspec_st <- ugarchspec(variance.model = list(model = 'tGARCH', garchOrder = c(1, 1)),
+                         mean.model = list(armaOrder = c(0, 0), include.mean = TRUE), distribution.model = 'std')
+tGARCHfit <- ugarchfit(tGARCHspec, data = data) # Threshold GARCH(1, 1) spec and fit 
+tGARCHfit <- ugarchfit(tGARCHspec_st, data = data)
+
+riskMetspec <- ugarchspec(variance.model=list(model = "iGARCH"),
+                          mean.model=list(armaOrder=c(0,0), include.mean=TRUE), fixed.pars=list(omega=0,alpha1=0.06,beta1=0.94))
+riskMetfit <- ugarchfit(riskMetspec, data = data)
 
 persistence(sGARCH_fit)
 
